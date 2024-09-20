2a71983c9319b4eb5692cc50c9a461969a524305
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2024-09-20 14:57:19 -0400

    feat: cleanup wrappers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
-import { EnvVarWarning } from '@/components/env-var-warning';
 import HeaderAuth from '@/components/header-auth';
 import { ThemeSwitcher } from '@/components/theme-switcher';
-import { hasEnvVars } from '@/utils/supabase/check-env-vars';
 import { GeistSans } from 'geist/font/sans';
 import { ThemeProvider } from 'next-themes';
 import Link from 'next/link';
 import './globals.css';
-import Providers from '@/components/Provider';
+import { ReactQueryClientProvider } from '@/components/ReactQueryClientProvider';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -24,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Providers>
+    <ReactQueryClientProvider>
       <html lang="en" className={GeistSans.className} suppressHydrationWarning>
         <body className="bg-background text-foreground">
           <ThemeProvider
@@ -33,13 +31,13 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <main className="min-h-screen flex flex-col items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <main className="min-h-screen flex flex-col">
+              <nav className="flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={'/'}>Squares App</Link>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <HeaderAuth />
                 </div>
               </nav>
               <div className="flex flex-col flex-grow w-full">{children}</div>
@@ -62,6 +60,6 @@ export default function RootLayout({
           </ThemeProvider>
         </body>
       </html>
-    </Providers>
+    </ReactQueryClientProvider>
   );
 }
---
+++ components/ClientWrapper.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { GridAPI } from '@/queries/grid.api';
+import useSupabaseBrowser from '@/utils/supabase/client';
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query';
+
+export default function ClientWrapper({
+  children,
+  gridId,
+}: {
+  children: React.ReactNode;
+  gridId: string;
+}) {
+  const supabase = useSupabaseBrowser();
+  const { data: gridInfo } = useQuery(GridAPI.getGridInfo(supabase, gridId));
+  return (
+    <div className="flex-1 space-y-4 xl:max-w-7xl xl:mx-auto pt-8 px-8">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">{gridInfo?.name}</h2>
+      </div>
+      {children}
+    </div>
+  );
+}
