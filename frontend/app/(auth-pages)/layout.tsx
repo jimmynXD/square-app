2b7762afdb7e0dfc8ddbd16502ad70862f212695
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2024-09-21 00:40:17 -0400

    feat: fe dir (#9)

---
+++ frontend/app/(auth-pages)/layout.tsx
@@ -0,0 +1,11 @@
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="justify-center items-center flex flex-col flex-grow">
+      {children}
+    </div>
+  );
+}
---
+++ frontend/app/(auth-pages)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { signInAction } from '@/app/actions';
+import GoogleSignInButton from '@/components/GoogleSignInButton';
+
+export default function Login() {
+  return (
+    <form action={signInAction} className="">
+      <GoogleSignInButton />
+    </form>
+  );
+}
