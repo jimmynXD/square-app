22039e8dd9c9e03f711fa964f5733ba907a56bc9
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2024-09-28 22:28:56 -0400

    feat: api v2

--- frontend/queries/grid.api.ts
+++ frontend/queries/grid.api.ts
@@ -1,17 +1,24 @@
 import { TablesInsert, TablesUpdate } from '@/utils/generated/database.types';
 import { TypedSupabaseClient } from '@/utils/types';
+import { getCurrentISODate } from '@/utils/utils';
 
 export const GridAPI = {
   getAll: (client: TypedSupabaseClient, userId: string) => {
     return client
       .from('grids')
-      .select('*')
+      .select('*, nfl_schedule(name, short_name, event_id, date)')
       .eq('user_id', userId)
       .order('created_at', { ascending: false });
   },
+
   getGridInfo: (client: TypedSupabaseClient, gridId: string) => {
-    return client.from('grids').select('*').eq('uuid', gridId).single();
+    return client
+      .from('grids')
+      .select('*, nfl_schedule(name, short_name, event_id, date)')
+      .eq('uuid', gridId)
+      .single();
   },
+
   countEmptyCells: (client: TypedSupabaseClient, gridId: string) => {
     return client
       .from('grid_cells')
@@ -57,13 +64,15 @@ export const GridAPI = {
     userId: string,
     gridName: string,
     numCols: number = 10,
-    numRows: number = 10
+    numRows: number = 10,
+    eventId: string
   ) => {
     const insertData: TablesInsert<'grids'> = {
       user_id: userId,
       name: gridName,
       num_cols: numCols,
       num_rows: numRows,
+      event_id: eventId,
     };
     return client.from('grids').insert(insertData).select().single();
   },
@@ -140,5 +149,85 @@ export const GridAPI = {
       .select('*')
       .eq('grid_id', gridId)
       .order('col', { ascending: true });
+  },
+  getSchedule: (client: TypedSupabaseClient) => {
+    const currentDate = getCurrentISODate();
+    return client
+      .from('nfl_schedule')
+      .select('*')
+      .gte('date', currentDate)
+      .order('date', { ascending: true });
+  },
+  getEvent: (client: TypedSupabaseClient, eventId: string) => {
+    return client
+      .from('nfl_schedule')
+      .select('*')
+      .eq('event_id', eventId)
+      .single();
+  },
+  getGame: (client: TypedSupabaseClient, eventId: string) => {
+    return (
+      client
+        .from('nfl_scores')
+        //   .select('*, nfl_schedule(home_team_id, away_team_id)')
+        .select(
+          `
+        *,
+        nfl_schedule (
+          home_team:teams!nfl_schedule_home_team_id_fkey(name, abbreviation),
+          away_team:teams!nfl_schedule_away_team_id_fkey(name, abbreviation)
+        )
+      `
+        )
+        .eq('event_id', eventId)
+        .single()
+    );
+  },
+  v2: {
+    getCellAssignments: (client: TypedSupabaseClient, gridId: number) => {
+      return client.from('cell_assignments').select('*').eq('grid_id', gridId);
+    },
+    createCellAssignments: (
+      client: TypedSupabaseClient,
+      gridId: number,
+      numCols: number,
+      numRows: number
+    ) => {
+      const cellsToInsert = [];
+      const rowValues = Array.from({ length: numRows }, (_, i) => i).sort(
+        () => Math.random() - 0.5
+      );
+      const colValues = Array.from({ length: numCols }, (_, i) => i).sort(
+        () => Math.random() - 0.5
+      );
+
+      for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+          cellsToInsert.push({
+            grid_id: gridId,
+            row_index: row,
+            col_index: col,
+            assigned_row_value: rowValues[row],
+            assigned_col_value: colValues[col],
+            assigned_value: null, // All assigned values are null
+          });
+        }
+      }
+      return client.from('cell_assignments').insert(cellsToInsert);
+    },
+    updateCellAssignment: (
+      client: TypedSupabaseClient,
+      gridId: number,
+      rowIndex: number,
+      colIndex: number,
+      assignedValue: string
+    ) => {
+      return client
+        .from('cell_assignments')
+        .update({ assigned_value: assignedValue })
+        .eq('grid_id', gridId)
+        .eq('row_index', rowIndex)
+        .eq('col_index', colIndex);
+    },
   },
 };
