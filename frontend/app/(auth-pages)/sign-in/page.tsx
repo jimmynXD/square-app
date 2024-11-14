f5611761f2cc4d2f447b31e26bf199e567f2afd9
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2024-11-14 03:27:01 -0500

    feat: square lord

--- frontend/app/(auth-pages)/sign-in/page.tsx
+++ frontend/app/(auth-pages)/sign-in/page.tsx
@@ -1,10 +1,30 @@
 import { signInAction } from '@/app/actions';
 import GoogleSignInButton from '@/components/GoogleSignInButton';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Siren } from 'lucide-react';
 
-export default function Login() {
+export default function Login({
+  searchParams,
+}: {
+  searchParams: { error?: string };
+}) {
   return (
-    <form action={signInAction} className="">
-      <GoogleSignInButton />
-    </form>
+    <div className="flex flex-col items-center gap-4">
+      {searchParams.error && (
+        <Alert>
+          <Siren className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>
+            {searchParams.error === 'access_denied'
+              ? 'Sign in was cancelled'
+              : searchParams.error}
+          </AlertDescription>
+        </Alert>
+      )}
+
+      <form action={signInAction} className="">
+        <GoogleSignInButton />
+      </form>
+    </div>
   );
 }
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -36,25 +36,17 @@ export default function RootLayout({
               <nav className="flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={'/'}>Squares App</Link>
+                    <Link href={'/'}>SquareLord</Link>
                   </div>
-                  <HeaderAuth />
+                  <div className="flex gap-5 items-center">
+                    <ThemeSwitcher />
+                    <HeaderAuth />
+                  </div>
                 </div>
               </nav>
               <div className="flex flex-col flex-grow w-full">{children}</div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{' '}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
                 <ThemeSwitcher />
               </footer>
             </main>
